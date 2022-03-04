7c559ac3f1d107f16f8c662602418ef5e3e00da2
mahmoud alhamaky <mahmoud.alhamaky@gmail.com>
2022-03-04 14:04:19 +0200

    it is lab 1, python refreshment,inputs from users & use it  Changes to be committed:

---
+++ lab1.py
@@ -0,0 +1,15 @@
+# read 1st number from the user 
+num1 = input("pleas enter your 1st number: ")
+# read 2ND number from the user
+num2 = input("pleas enter your 2nd number: ")
+# convert number from string to float
+fnum1 = float (num1)
+fnum2 = float (num2)
+# print sum
+print ("sum is " , fnum1 + fnum2)
+# print sub
+print ("sub is " , fnum1 - fnum2)
+# print mulitplication
+print ("multiplication is " , fnum1 * fnum2)
+# print division
+print ("division is " , fnum1 / fnum2)
